bf99fea5fb4a31d9c908d83e89b3d612a6cc7e15
iosypov <ivan.osypov@redocly.com>
2022-10-18 16:55:46 +0300

    chore: update sdk

--- src/hooks/useFeedbackApi.js
+++ src/hooks/useFeedbackApi.js
@@ -1,24 +1,19 @@
-import {
-  Client, FeedbackController,
-} from 'blips-and-chitz-feedback-api-sdk';
+import { Client, FeedbackController } from "blips-and-chitz-feedback-api-sdk"
 const client = new Client({
   timeout: 0,
-  xRapidAPIKey: '637316bb60msh72029ce58a98a83p19d6d5jsnb9fefc607f61',
+  xRapidAPIKey: "637316bb60msh72029ce58a98a83p19d6d5jsnb9fefc607f61",
+  xAPIKEY: "LgcVsiwdXDt1AYSqtf5fbt4GYhk2jGGW",
+  xRapidAPIHost: "feedback-api5.p.rapidapi.com",
 })
-const publicController = new FeedbackController(client);
+const publicController = new FeedbackController(client)
 function useFeedbackApi() {
   function create(sentiment, page) {
     publicController.createFeedback({
       sentiment,
       page,
-      id: "",
-      createdAt: "",
-      userIP: "",
-      tenantId: "",
-      userAgent: ""
-    });
+    })
   }
-  return { create };
+  return { create }
 }
 
-export default useFeedbackApi;
+export default useFeedbackApi
