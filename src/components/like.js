c3682eae7decab0bc4dee2784fd7c6cc05ff5c9e
iosypov <ivan.osypov@redocly.com>
2022-10-18 10:19:11 +0300

    make save into localStorage

--- src/components/like.js
+++ src/components/like.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { BiLike, BiDislike } from "react-icons/bi"
 
@@ -6,18 +6,35 @@ import useFeedbackApi from "../hooks/useFeedbackApi"
 
 const Like = ({ page }) => {
   const feedbackApi = useFeedbackApi()
+  const [likeState, setLikeState] = useState(
+    JSON.parse(localStorage.getItem(`${page}_like`))
+  )
   const handleLikeClick = React.useCallback(
     like => {
+      setLikeState(like)
+      localStorage.setItem(`${page}_like`, like)
       feedbackApi.create(like, page)
     },
     [feedbackApi, page]
   )
   return (
     <div style={{ display: "flex", flexDirection: "row", fontSize: "20px" }}>
-      <div style={{ paddingRight: "10px" }}>
+      <div
+        style={{
+          paddingRight: "10px",
+          color: likeState ? "blue" : "black",
+          cursor: "pointer",
+        }}
+      >
         <BiLike onClick={handleLikeClick.bind(null, true)} />
       </div>
-      <div>
+      <div
+        style={{
+          paddingRight: "10px",
+          color: likeState === false ? "blue" : "black",
+          cursor: "pointer",
+        }}
+      >
         <BiDislike onClick={handleLikeClick.bind(null, false)} />
       </div>
     </div>
